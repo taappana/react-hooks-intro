8e65d9af7a43a011973eb5458a5d1fa490ef7f08
taappana <herranen.jani@gmail.com>
2019-03-13 20:46:12 +0200

    Change useState id to number

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Character from './components/Character';
 const App = props => {
   const [destroyed, setDestroyed] = useState(false);
 
-  const [selectedCharacter, setSelectedCharacter] = useState('1');
+  const [selectedCharacter, setSelectedCharacter] = useState(1);
 
   const [chosenSide, setChosenSide] = useState('light');
 
