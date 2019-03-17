2971e1866a8082c1db6143b71c0f424416d045ed
taappana <herranen.jani@gmail.com>
2019-03-17 15:03:52 +0200

    Created an http hook to be used for http requests

--- src/components/CharPicker.js
+++ src/components/CharPicker.js
@@ -1,49 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import { useHttp } from '../hooks/http';
 
 import './CharPicker.css';
 
 const CharPicker = props => {
-  const [loadedChars, setLoadedChars] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, fetchedData] = useHttp('https://swapi.co/api/people', []);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch('https://swapi.co/api/people')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch.');
-        }
-        return response.json();
-      })
-      .then(charData => {
-        const selectedloadedChars = charData.results.slice(0, 5);
-        setIsLoading(false);
-        setLoadedChars(selectedloadedChars.map((char, index) => ({
-          name: char.name,
-          id: index + 1
-        }))
-        );
-      })
-      .catch(err => {
-        console.log(err);
-        setIsLoading(false);
-      });
-  }, []);
+  const selectedCharacters = fetchedData 
+  ? fetchedData.results.slice(0, 5).map((char, index) => ({
+      name: char.name,
+      id: index + 1
+    })) 
+  : [];
 
   let content = <p>Loading characters...</p>;
 
-  if (
-    !isLoading &&
-    loadedChars &&
-    loadedChars.length > 0
-  ) {
+  if (!isLoading && selectedCharacters && selectedCharacters.length > 0) {
     content = (
       <select
         onChange={props.onCharSelect}
         value={props.selectedChar}
         className={props.side}
       >
-        {loadedChars.map(char => (
+        {selectedCharacters.map(char => (
           <option key={char.id} value={char.id}>
             {char.name}
           </option>
@@ -52,7 +32,7 @@ const CharPicker = props => {
     );
   } else if (
     !isLoading &&
-    (!loadedChars || loadedChars.length === 0)
+    (!selectedCharacters || selectedCharacters.length === 0)
   ) {
     content = <p>Could not fetch any data.</p>;
   }
